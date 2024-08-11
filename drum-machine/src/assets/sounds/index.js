39b1bbe7bc6320b993615776792d3b0f0a8b7fa5
Glorie Ramazani <glorieramazani@gmail.com>
2024-08-10 20:37:17 -0400

    Export assets to single index.js file

---
+++ drum-machine/src/assets/sounds/index.js
@@ -0,0 +1,21 @@
+import HiHat from "./Cev_H2.mp3";
+import OverHat from "./Dsc_Oh.mp3";
+import Heater1 from "./Heater-1.mp3";
+import Heater2 from "./Heater-2.mp3";
+import Heater3 from "./Heater-3.mp3";
+import Heater4 from "./Heater-4.mp3";
+import Heater6 from "./Heater-6.mp3";
+import KicknHat from "./Kick_n_Hat.mp3";
+import RP4_KICK from "./RP4_KICK.mp3";
+
+export {
+  HiHat,
+  OverHat,
+  Heater1,
+  Heater2,
+  Heater3,
+  Heater4,
+  Heater6,
+  KicknHat,
+  RP4_KICK,
+};
---
+++ drum-machine/src/constants/index.js
@@ -0,0 +1,11 @@
+import {
+  HiHat,
+  OverHat,
+  Heater1,
+  Heater2,
+  Heater3,
+  Heater4,
+  Heater6,
+  KicknHat,
+  RP4_KICK,
+} from "./assets/sounds";
