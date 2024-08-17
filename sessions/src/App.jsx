473b1a74da8f07544ec73e07336c41483a8d33ba
Glorie Ramazani <glorieramazani@gmail.com>
2024-08-16 23:54:39 -0400

    Add code for timer functionality

--- sessions/src/App.jsx
+++ sessions/src/App.jsx
@@ -1,5 +1,125 @@
+import { useState, useEffect, useRef } from "react";
+
 function App() {
-  return <h1>Sessions by Gloire</h1>;
+  // units are in seconds
+  const DEFAULT = {
+    session: 25 * 60,
+    break: 5 * 60,
+  };
+  const timerRef = useRef(null);
+
+  const [sessionLength, setSessionLength] = useState(DEFAULT.session);
+  const [breakLength, setBreakLength] = useState(DEFAULT.break);
+  const [pauseTimer, setPauseTimer] = useState(true);
+  const [count, setCount] = useState(DEFAULT.session);
+  // let pauseTimer = false;
+
+  const handleReset = () => {
+    console.log("Reseting timer");
+    // stop timer
+    clearInterval(timerRef.current);
+    // console.log(timerRef.current);
+
+    // reset variables
+    setSessionLength(DEFAULT.session);
+    setBreakLength(DEFAULT.break);
+    setCount(DEFAULT.session);
+    timerRef.current = null;
+  };
+
+  const handleStart = () => {
+    if (!timerRef.current) {
+      setPauseTimer(false);
+    } else {
+      setPauseTimer(!pauseTimer);
+    }
+
+    // pauseTimer = !pauseTimer;
+  };
+
+  const breakDecrement = () => {
+    const newBreakLength = breakLength - 1;
+    setBreakLength(newBreakLength <= 0 ? breakLength : newBreakLength);
+  };
+
+  const breakIncrement = () => {
+    const newBreakLength = breakLength + 1;
+    setBreakLength(newBreakLength > 60 ? breakLength : newBreakLength);
+  };
+
+  const sessionDecrement = () => {
+    const newSessionLength = sessionLength - 1;
+    setSessionLength(newSessionLength <= 0 ? sessionLength : newSessionLength);
+  };
+
+  const sessionIncrement = () => {
+    const newSessionLength = sessionLength + 1;
+    setSessionLength(newSessionLength > 60 ? sessionLength : newSessionLength);
+  };
+
+  useEffect(() => {
+    // Set up the interval
+    if (timerRef.current) clearInterval(timerRef.current);
+    const interval = setInterval(() => {
+      pauseTimer ? null : setCount((prevCount) => prevCount - 1);
+      console.log("Pause timer:", pauseTimer);
+    }, 1000); // Update every second
+    timerRef.current = interval;
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, [pauseTimer]); // Empty dependency array means this effect runs once on mount
+
+  return (
+    <div id="wrapper">
+      <h1>Sessions by Gloire</h1>
+
+      <div id="top-container">
+        <div id="break-label">
+          <h2>Break Length</h2>
+          <div id="break-length">
+            <h3>{breakLength / 60}</h3>
+          </div>
+          <button id="break-increment" onClick={breakIncrement}>
+            UP
+          </button>
+          <button id="break-decrement" onClick={breakDecrement}>
+            DOWN
+          </button>
+        </div>
+
+        <div id="session-label">
+          <h2>Session Length</h2>
+          <div id="session-length">
+            <h3>{sessionLength / 60}</h3>
+          </div>
+          <button id="session-increment" onClick={sessionIncrement}>
+            UP
+          </button>
+          <button id="session-decrement" onClick={sessionDecrement}>
+            DOWN
+          </button>
+        </div>
+      </div>
+
+      <div id="timer-label">Session</div>
+      <div id="time-left">
+        Paused or running (mm:ss)
+        <br />
+        <span>
+          {Math.floor(count / 60)}:
+          {Math.floor(count % 60)
+            .toString()
+            .padStart(2, "0")}
+        </span>
+      </div>
+      <button id="start_stop" onClick={handleStart}>
+        START/STOP
+      </button>
+      <button id="reset" onClick={handleReset}>
+        RESET
+      </button>
+    </div>
+  );
 }
 
 export default App;
