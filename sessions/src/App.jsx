cb62cc756a30eabffa7264113e308ec16ee14190
Glorie Ramazani <glorieramazani@gmail.com>
2024-08-17 22:26:10 -0400

    Add functionality to swtich between Sessions and Breaks.
    Fix increment and decrement issue

--- sessions/src/App.jsx
+++ sessions/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { beep as alertSound } from "./assests/sounds";
 
 function App() {
   // units are in seconds
@@ -7,16 +8,25 @@ function App() {
     break: 5 * 60,
   };
   const timerRef = useRef(null);
+  const MODE = {
+    SESSION: "session",
+    BREAK: "break",
+  };
 
   const [sessionLength, setSessionLength] = useState(DEFAULT.session);
   const [breakLength, setBreakLength] = useState(DEFAULT.break);
   const [pauseTimer, setPauseTimer] = useState(true);
   const [count, setCount] = useState(DEFAULT.session);
+  const [mode, setMode] = useState(MODE.SESSION);
+  const alert = document.getElementById("beep");
 
   const handleReset = () => {
-    console.log("Reseting timer");
     // stop timer
     clearInterval(timerRef.current);
+
+    // stop audio
+    alert.pause();
+    alert.currentTime = 0;
 
     // reset variables
     setSessionLength(DEFAULT.session);
@@ -31,36 +41,92 @@ function App() {
   };
 
   const breakDecrement = () => {
-    const newBreakLength = breakLength - 1;
+    const newBreakLength = breakLength - 60;
     setBreakLength(newBreakLength <= 0 ? breakLength : newBreakLength);
   };
 
   const breakIncrement = () => {
-    const newBreakLength = breakLength + 1;
-    setBreakLength(newBreakLength > 60 ? breakLength : newBreakLength);
+    const newBreakLength = breakLength + 60;
+    setBreakLength(newBreakLength > 60 * 60 ? breakLength : newBreakLength);
   };
 
   const sessionDecrement = () => {
-    const newSessionLength = sessionLength - 1;
-    setSessionLength(newSessionLength <= 0 ? sessionLength : newSessionLength);
+    const newSessionLength = sessionLength - 60;
+    if (newSessionLength <= 0) {
+      setSessionLength(sessionLength);
+      setCount(sessionLength);
+    } else {
+      setSessionLength(newSessionLength);
+      setCount(newSessionLength);
+    }
   };
 
   const sessionIncrement = () => {
-    const newSessionLength = sessionLength + 1;
-    setSessionLength(newSessionLength > 60 ? sessionLength : newSessionLength);
+    const newSessionLength = sessionLength + 60;
+    if (newSessionLength > 60 * 60) {
+      setSessionLength(sessionLength);
+      setCount(sessionLength);
+    } else {
+      setSessionLength(newSessionLength);
+      setCount(newSessionLength);
+    }
   };
 
   useEffect(() => {
+    const playAlertSound = () => {
+      try {
+        alert.play();
+      } catch (e) {
+        console.log(e);
+      }
+    };
     // Set up the interval
     if (timerRef.current) clearInterval(timerRef.current);
+
+    // switch between session and break
+    if (count === 0) {
+      // switch
+      playAlertSound();
+      if (mode === MODE.BREAK) {
+        setMode(() => MODE.SESSION);
+        setCount(sessionLength);
+      } else {
+        setMode(() => MODE.BREAK);
+        setCount(breakLength);
+      }
+      return;
+    }
+
     const interval = setInterval(() => {
       pauseTimer ? null : setCount((prevCount) => prevCount - 1);
     }, 1000); // Update every second
     timerRef.current = interval;
     // Clean up the interval on component unmount
     return () => clearInterval(interval);
-  }, [pauseTimer]); // Empty dependency array means this effect runs once on mount
+  }, [
+    DEFAULT.break,
+    DEFAULT.session,
+    MODE.BREAK,
+    MODE.SESSION,
+    alert,
+    breakLength,
+    count,
+    mode,
+    pauseTimer,
+    sessionLength,
+  ]); // Empty dependency array means this effect runs once on mount
 
+  const setMinutes = (count) => {
+    return Math.floor(count / 60)
+      .toString()
+      .padStart(2, "0");
+  };
+
+  const setSeconds = (count) => {
+    return Math.floor(count % 60)
+      .toString()
+      .padStart(2, "0");
+  };
   return (
     <div id="wrapper">
       <h1>Sessions by Gloire</h1>
@@ -69,7 +135,7 @@ function App() {
         <div id="break-label">
           <h2>Break Length</h2>
           <div id="break-length">
-            <h3>{breakLength / 60}</h3>
+            <h3>{Math.floor(breakLength / 60)}</h3>
           </div>
           <button id="break-increment" onClick={breakIncrement}>
             UP
@@ -82,7 +148,7 @@ function App() {
         <div id="session-label">
           <h2>Session Length</h2>
           <div id="session-length">
-            <h3>{sessionLength / 60}</h3>
+            <h3>{Math.floor(sessionLength / 60)}</h3>
           </div>
           <button id="session-increment" onClick={sessionIncrement}>
             UP
@@ -93,16 +159,9 @@ function App() {
         </div>
       </div>
 
-      <div id="timer-label">Session</div>
+      <div id="timer-label">{mode.toUpperCase()}</div>
       <div id="time-left">
-        Paused or running (mm:ss)
-        <br />
-        <span>
-          {Math.floor(count / 60)}:
-          {Math.floor(count % 60)
-            .toString()
-            .padStart(2, "0")}
-        </span>
+        {setMinutes(count)}:{setSeconds(count)}
       </div>
       <button id="start_stop" onClick={handleStart}>
         START/STOP
@@ -110,6 +169,7 @@ function App() {
       <button id="reset" onClick={handleReset}>
         RESET
       </button>
+      <audio id="beep" src={alertSound}></audio>
     </div>
   );
 }
